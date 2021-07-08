9aea6059bb30496c39001faad390c3130b3425b6
doctorCrazy <npoudelp@gmail.com>
2021-07-08 22:08:03 +0545

    Code for arduino with temp input and system control added

---
+++ arduinoCode.ino
@@ -0,0 +1,81 @@
+#include <Adafruit_Sensor.h>
+
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHTPIN 3         // Pin connected to the DHT sensor
+// Uncomment the type of sensor in use:
+//#define DHTTYPE    DHT11     // DHT 11
+#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+// See guide for details on sensor wiring and usage:
+
+DHT_Unified dht(DHTPIN, DHTTYPE); 
+
+uint32_t delayMS;
+
+#include <LiquidCrystal.h>
+int relay_pin=13;
+int buzzer=2;
+float st=37.5;
+float D=28.5;
+float C=36.5;
+const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 =7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() { 
+pinMode(relay_pin,OUTPUT);
+pinMode(buzzer,OUTPUT);
+Serial.begin(9600);
+dht.begin();
+sensor_t sensor;
+dht.temperature().getSensor(&sensor);
+lcd.begin(16,2);
+lcd.print("    npoudelp");
+lcd.setCursor(0,2);
+lcd.print("      IKF");
+delay(2000);
+lcd.clear();
+lcd.print("Incubator v0.0.1");
+digitalWrite(relay_pin,HIGH);
+digitalWrite(buzzer,HIGH);
+delay(300);
+digitalWrite(relay_pin,LOW);
+digitalWrite(buzzer,LOW);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  delay(700);
+
+}
+
+void loop(){
+  lcd.begin(16,2);
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  float A=event.temperature; 
+  if(A>=st){
+    digitalWrite(relay_pin,HIGH);
+    } 
+  else{
+    digitalWrite(relay_pin,LOW);
+  }  
+  if(A >= D or A <= C){
+    digitalWrite(buzzer,HIGH);
+    }  
+  else{
+    digitalWrite(buzzer,LOW);
+    } 
+  lcd.print("Temp: "); 
+  lcd.print(A);
+
+
+  dht.humidity().getEvent(&event);
+  float B=event.relative_humidity;
+  lcd.setCursor(0,2);
+  lcd.print("Humi: ");    
+  lcd.print(B);
+
+  delay(2000);
+  lcd.clear(); 
+  
+  
+}
